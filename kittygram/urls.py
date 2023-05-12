da6c189a359f795e01e34fd830d5070f1840c621
JohnnyFes <johnny@list.ru>
2023-05-12 20:26:10 +0500

    Init commit

---
+++ kittygram/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+
+from cats.views import APICat, APICatDetail
+
+urlpatterns = [
+   path('cats/', APICat.as_view()),
+   path('cats/<int:pk>/', APICatDetail.as_view()),
+]
